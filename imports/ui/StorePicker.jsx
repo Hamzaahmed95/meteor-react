8001e8c9504834ed5dbc579495eec9c48eb16027
Hamzaahmed95 <hamzzza.ahmed95@gmail.com>
2016-08-24 23:00:37 +0500

    event with react router done

--- imports/ui/StorePicker.jsx
+++ imports/ui/StorePicker.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
-
+import  History from 'react-router';
 import h from './helpers.jsx';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
+import {createHistory} from 'history'
+
+const history = createHistory();
 export default class StorePicker extends Component {
+
+
+    goToStore(event){
+        event.preventDefault();
+        console.log("here");
+        var storeID = event.target.s.value;
+        console.log("now "+ storeID);
+        history.push('/signIn/' + storeID);
+        //event.target.s.value ='';
+    }
     render() {
         return (
-            <form className="center">
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
                 <h2>Please Enter a Store</h2>
-                <input type="text" ref="storeId" defaultValue={h.getFunNames()} required />
+                <input type="text" name="s" defaultValue={h.getFunNames()} required />
                 <input type="Submit" />
             </form>
+
         );
     }
 
